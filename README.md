1e9880b0fd02d59cf7dfab58d4dc5290b563b2ec
Dawid <dawid.ol1@o2.pl>
2023-05-19 13:53:04 +0200

    Lab 6 Fix

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# taw_L5 za pomocą funkcji console.log został wyświetlony tekst zawarty jako argument tej funkcji
-#
+# TAW Lab_6
---
+++ app/REST/routes.js
@@ -0,0 +1,9 @@
+import dataEndpoint from './data.endpoint';
+
+const routes = function (router)
+{
+ dataEndpoint(router);
+};
+
+export default routes;
+
