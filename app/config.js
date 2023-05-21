21c68ccf115507b5f288697cc4dc0c2a9fc2d035
Dawid <dawid.ol1@o2.pl>
2023-05-21 11:39:20 +0200

    Lab7

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3000,
-  databaseUrl: process.env.MONGODB_URI || '',
+  databaseUrl: process.env.MONGODB_URI || 'mongodb+srv://dawid:123@cluster0.azzlocs.mongodb.net/?retryWrites=true&w=majority',
   JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 
